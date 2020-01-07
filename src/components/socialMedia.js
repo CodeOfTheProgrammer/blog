514415d664018ed231742b02c110b2eb33d11b46
erturne <erturne@gmail.com>
2020-01-07 13:26:37 -0500

    Switch to react-icons

--- src/components/socialMedia.js
+++ src/components/socialMedia.js
@@ -4,9 +4,8 @@
 
 import React from 'react';
 import {graphql, useStaticQuery} from "gatsby";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faInstagram, faLinkedin, faRedditSquare, faTwitterSquare, faGithubSquare} from "@fortawesome/free-brands-svg-icons";
-import {faEnvelopeSquare} from "@fortawesome/free-solid-svg-icons";
+import {FaTwitterSquare, FaGithubSquare, FaRedditSquare, FaInstagram, FaLinkedin, FaEnvelopeSquare} from 'react-icons/fa';
+
 import styled from 'styled-components';
 
 const SocialMediaLinks = styled.div`
@@ -39,27 +38,27 @@ const SocialMedia = () => {
     return (
         <SocialMediaLinks className={'SocialMedia'}>
             <a className={'social-link'} href={`https://twitter.com/${social.twitter}`} title="Twitter" rel="noopener noreferrer" target="_blank">
-                <FontAwesomeIcon icon={faTwitterSquare} />
+                <FaTwitterSquare />
             </a>
             {` `}
             <a className={'social-link'} href={`https://github.com/${social.github}`} title="Github" rel="noopener noreferrer" target="_blank">
-                <FontAwesomeIcon icon={faGithubSquare} />
+                <FaGithubSquare />
             </a>
             {` `}
             <a className={'social-link'} href={`https://reddit.com/u/${social.reddit}`} title="Reddit" rel="noopener noreferrer" target="_blank">
-                <FontAwesomeIcon icon={faRedditSquare} />
+                <FaRedditSquare />
             </a>
             {` `}
             <a className={'social-link'} href={`https://instagram.com/${social.instagram}`} title="Instagram" rel="noopener noreferrer" target="_blank">
-                <FontAwesomeIcon icon={faInstagram} />
+                <FaInstagram />
             </a>
             {` `}
             <a className={'social-link'} href={`https://www.linkedin.com/in/${social.linkedin}`} title="LinkedIn" rel="noopener noreferrer" target="_blank">
-                <FontAwesomeIcon icon={faLinkedin} />
+                <FaLinkedin />
             </a>
             {` `}
             <a className={'social-link'} href={`mailto:codeoftheprogrammer@gmail.com`} title="Email" rel="noopener noreferrer" target="_blank">
-                <FontAwesomeIcon icon={faEnvelopeSquare} />
+                <FaEnvelopeSquare />
             </a>
         </SocialMediaLinks>
     );
