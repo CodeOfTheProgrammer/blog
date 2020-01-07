42d7bc60c8e2f57a6b3b35100e2a5f87cf36be96
erturne <erturne@gmail.com>
2020-01-06 22:43:45 -0500

    Add Styled Components library and improve styling of the social media icons

--- src/components/socialMedia.js
+++ src/components/socialMedia.js
@@ -7,6 +7,15 @@ import {graphql, useStaticQuery} from "gatsby";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faInstagram, faLinkedin, faRedditSquare, faTwitterSquare} from "@fortawesome/free-brands-svg-icons";
 import {faEnvelopeSquare} from "@fortawesome/free-solid-svg-icons";
+import styled from 'styled-components';
+
+const SocialMediaLinks = styled.div`
+  font-size: 1.5rem;
+  .social-link {
+    color: #007acc;
+    box-shadow: none;
+  }
+`;
 
 const SocialMedia = () => {
   const data = useStaticQuery(graphql`
@@ -27,27 +36,27 @@ const SocialMedia = () => {
     const { social } = data.site.siteMetadata;
 
     return (
-        <div className={'SocialMedia'}>
-            <a className={'social-link'} href={`https://twitter.com/${social.twitter}`} target="_blank">
+        <SocialMediaLinks className={'SocialMedia'}>
+            <a className={'social-link'} href={`https://twitter.com/${social.twitter}`} title="Twitter" rel="noopener noreferrer" target="_blank">
                 <FontAwesomeIcon icon={faTwitterSquare} />
             </a>
             {` `}
-            <a className={'social-link'} href={`https://reddit.com/u/${social.reddit}`} target="_blank">
+            <a className={'social-link'} href={`https://reddit.com/u/${social.reddit}`} title="Reddit" rel="noopener noreferrer" target="_blank">
                 <FontAwesomeIcon icon={faRedditSquare} />
             </a>
             {` `}
-            <a className={'social-link'} href={`https://instagram.com/${social.instagram}`} target="_blank">
+            <a className={'social-link'} href={`https://instagram.com/${social.instagram}`} title="Instagram" rel="noopener noreferrer" target="_blank">
                 <FontAwesomeIcon icon={faInstagram} />
             </a>
             {` `}
-            <a className={'social-link'} href={`https://www.linkedin.com/in/${social.linkedin}`} target="_blank">
+            <a className={'social-link'} href={`https://www.linkedin.com/in/${social.linkedin}`} title="LinkedIn" rel="noopener noreferrer" target="_blank">
                 <FontAwesomeIcon icon={faLinkedin} />
             </a>
             {` `}
-            <a className={'social-link'} href={`mailto:codeoftheprogrammer@gmail.com`} target="_blank">
+            <a className={'social-link'} href={`mailto:codeoftheprogrammer@gmail.com`} title="Email" rel="noopener noreferrer" target="_blank">
                 <FontAwesomeIcon icon={faEnvelopeSquare} />
             </a>
-        </div>
+        </SocialMediaLinks>
     );
 };
 
