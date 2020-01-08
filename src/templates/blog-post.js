567c5ee11b0207639b0ab977a922bc3c15f7f737
erturne <erturne@gmail.com>
2020-01-08 17:49:28 -0500

    Convert src/templates/blog-post.js to use styled-components

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,93 +1,94 @@
 import React from "react"
 import Disqus from "disqus-react";
-import { Link, graphql } from "gatsby"
+import {Link, graphql} from "gatsby"
 import styled from 'styled-components';
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm, scale } from "../utils/typography"
+import {rhythm, scale} from "../utils/typography"
 
-const Hr = styled.hr`
+const Divider = styled.hr`
     margin-bottom: ${() => rhythm(1)};
 `;
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark;
-    console.log(this.props);
-    const siteTitle = this.props.data.site.siteMetadata.title;
-    const { previous, next } = this.props.pageContext;
+const PostTitle = styled.h1`
+    margin-top: ${() => rhythm(1)};
+    margin-bottom: 0;
+`;
+
+const PostDate = styled.p`
+    font-size: ${() => scale(-1 / 5).fontSize};
+    line-height: ${() => scale(-1 / 5).lineHeight};
+    display: block;
+    margin-bottom: ${() => rhythm(1)};
+`;
+
+const PostNav = styled(({previous, next, className}) => {
+    return (
+        <nav className={className}>
+            <ul>
+                <li>
+                    {previous && (
+                        <Link to={previous.fields.slug} rel="prev">
+                            ← {previous.frontmatter.title}
+                        </Link>
+                    )}
+                </li>
+                <li>
+                    {next && (
+                        <Link to={next.fields.slug} rel="next">
+                            {next.frontmatter.title} →
+                        </Link>
+                    )}
+                </li>
+            </ul>
+        </nav>
+    )
+})`
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        list-style: none;
+        padding: 0;
+    }
+`;
+
+const BlogPostTemplate = ({data, pageContext, location, className}) => {
+    const post = data.markdownRemark;
+    const siteTitle = data.site.siteMetadata.title;
+    const {previous, next} = pageContext;
     const disqusShortName = 'codeoftheprogrammer';
     const disqusConfig = {
-        url: this.props.location.href,
+        url: location.href,
         identifier: post.id,
         title: post.frontmatter.title,
     };
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-        />
-        <article>
-          <header>
-            <h1
-              style={{
-                marginTop: rhythm(1),
-                marginBottom: 0,
-              }}
-            >
-              {post.frontmatter.title}
-            </h1>
-            <p
-              style={{
-                ...scale(-1 / 5),
-                display: `block`,
-                marginBottom: rhythm(1),
-              }}
-            >
-              {post.frontmatter.date}
-            </p>
-          </header>
-          <section dangerouslySetInnerHTML={{ __html: post.html }} />
-          <Hr />
-          <footer>
-            <Bio />
-            <Hr />
-            <Disqus.DiscussionEmbed shortname={disqusShortName} config={disqusConfig}/>
-          </footer>
-        </article>
 
-        <nav>
-          <ul
-            style={{
-              display: `flex`,
-              flexWrap: `wrap`,
-              justifyContent: `space-between`,
-              listStyle: `none`,
-              padding: 0,
-            }}
-          >
-            <li>
-              {previous && (
-                <Link to={previous.fields.slug} rel="prev">
-                  ← {previous.frontmatter.title}
-                </Link>
-              )}
-            </li>
-            <li>
-              {next && (
-                <Link to={next.fields.slug} rel="next">
-                  {next.frontmatter.title} →
-                </Link>
-              )}
-            </li>
-          </ul>
-        </nav>
-      </Layout>
+    return (
+        <Layout location={location} title={siteTitle} className={className}>
+            <SEO
+                title={post.frontmatter.title}
+                description={post.frontmatter.description || post.excerpt}
+            />
+            <article>
+                <header>
+                    <PostTitle>{post.frontmatter.title}</PostTitle>
+                    <PostDate>{post.frontmatter.date}</PostDate>
+                </header>
+                <section dangerouslySetInnerHTML={{__html: post.html}}/>
+                <Divider/>
+                <footer>
+                    <Bio/>
+                    <Divider/>
+                    <Disqus.DiscussionEmbed shortname={disqusShortName} config={disqusConfig}/>
+                </footer>
+            </article>
+
+            <PostNav previous={previous} next={next} />
+        </Layout>
     )
-  }
-}
+};
 
 export default BlogPostTemplate
 
@@ -109,4 +110,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
