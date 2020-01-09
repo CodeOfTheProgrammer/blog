1b2c7cb01c756b9563e6b6ea39ef8d427d9c3fe7
erturne <erturne@gmail.com>
2020-01-09 15:17:00 -0500

    Add google analytics tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-155965722-1`,
       },
     },
     `gatsby-plugin-feed`,
